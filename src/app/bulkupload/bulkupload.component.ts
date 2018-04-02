563e8042b57769be32111ff02e3353befe0cb4fe
ashish051321 <ashish.25jl@gmail.com>
2018-04-02 10:58:12 -0400

    excel uploda to firebase as json is working

--- src/app/bulkupload/bulkupload.component.ts
+++ src/app/bulkupload/bulkupload.component.ts
@@ -27,23 +27,47 @@ export class BulkuploadComponent implements OnInit {
     console.log($);
     readXlsxFile(files[0], { schema: mySchema }).then(({ rows, errors }) => {
 
-        rows.forEach(item => {
-           Object.defineProperty(this.mydata,(item.empid),{value: item, writable:true,enumerable:true});
-        });
+      rows.forEach(item => {
+        Object.defineProperty(this.mydata, (item.empid), { value: item, writable: true, enumerable: true });
+      });
 
 
+      // console.log(this.mydata);
+      // now we have got the excel prased as JSON, ican preprocess it and go ahead with saving into the Firebase database.
+      this.preProcessMyJSON();
       console.log(this.mydata);
-      this.af.list('/').set('empDB', this.mydata).then((success:any) => {
-        alert("data inserted successfully"+success.message);
+
+
+      this.af.list('/').set('empDB', this.mydata).then((msg: any) => {
+        console.log("data inserted successfully");
+        // console.log(msg);
       });
     });
 
 
-    // now we have got the excel prased as JSON, ican preprocess it and go ahead with saving into the Firebase database.
-    // this.preProcessMyJSON();
 
 
 
   }
+
+  preProcessMyJSON() {
+
+    let masterkeys = Object.keys(mySchema);
+
+    Object.keys(this.mydata).forEach((key) => {
+      // key: the name of the object key
+      // index: the ordinal position of the key within the object 
+      let item = this.mydata[key];
+      let itemkeys = Object.keys(item);
+      masterkeys.forEach(mkitem => {
+        if (!itemkeys.includes(mkitem)) {
+          Object.defineProperty(item, mkitem, { value: "null", writable: true, enumerable: true });
+        }
+      });
+
+    });
+
+  }
+
 
 }
